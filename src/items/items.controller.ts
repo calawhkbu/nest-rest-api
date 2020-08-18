8c8930c7fdc93c64fd19c13c8c742fb1c08331f6
calvin Law <calvin.law@swivelsoftware.com>
2020-08-18 14:53:18 +0800

    get post put delete

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post,Body } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Controller, Get, Post,Body, Req, Res, Param, Put, Delete } from '@nestjs/common';
 import {CreateItemDto} from './dto/create-item.dto';
-
+import {Request,Response} from 'express';
 
 @Controller('items')
 export class ItemsController {
     @Get()
     findAll():string{
-        return 'Get all items';
+        return "get all items";
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id):string{
+        return `Item ${id}`;
     }
 
     @Post()
@@ -15,4 +21,21 @@ export class ItemsController {
         Name: ${item.name} Desc:${item.description}
          Qty:${item.qty}`;
     }
+
+    @Delete(':id')
+    delete(@Param('id') id){
+        return `Deleted ${id}`;
+    }
+
+    @Put(':id')
+    update(@Body() updateItem:CreateItemDto,@Param('id') id):string{
+        return `Updated Item ${id}--
+        Name:${updateItem.name}
+        Description:${updateItem.description}
+        Qty:${updateItem.qty}
+        `;
+        
+
+    }
 }
+
